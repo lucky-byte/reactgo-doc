4ec810b0e955395e5b0c7e33f7d43e3394b35282
xiaohu <xiaohu@lucky-byte.com>
2022-02-28 19:33:04 +0800

    改进文档

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,21 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <div className='row'>
           <div className="col col--5">
-            <h1 className="hero__title">{siteConfig.title}</h1>
-            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <h1 className={clsx("hero__title", styles.title)}>REACT + GO</h1>
+            <img className={styles.logo} alt='React'
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+            />
+            <img className={styles.logo} alt='Go'
+              src='https://ucarecdn.com/157c4486-7979-4103-a59b-8be08b54a84a/'
+            />
+            <p className={clsx("hero__subtitle", styles.subtitle)}>
+              快速交付你的下一个优质项目
+            </p>
             <div className={styles.buttons}>
               <Link className="button button--secondary button--lg"
                 to="/docs/intro">
@@ -26,7 +32,7 @@ function HomepageHeader() {
               </Link>
             </div>
             <p className={clsx('margin-top--xl', styles.secondary)}>
-              如果你觉得 ReactGO 还不错，请打开
+              如果你觉得 ReactGO 还不错，请到
               <Link href='https://github.com/lucky-byte/reactgo' target='_blank'>
                 项目主页
               </Link>
@@ -44,6 +50,7 @@ function HomepageHeader() {
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <Layout title={`${siteConfig.title}`} description="ReactGo 文档">
       <HomepageHeader />
